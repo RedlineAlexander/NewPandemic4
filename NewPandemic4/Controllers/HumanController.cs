24afa381412f2f91e974238bedf176782514a6c7
RedlineAlexander <redlinerailmetro@gmail.com>
2020-06-01 16:04:35 +0300

    HW5

--- NewPandemic4/Controllers/HumanController.cs
+++ NewPandemic4/Controllers/HumanController.cs
@@ -5,9 +5,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 /*
- * В HumanController добавьте action Country(если он у вас уже есть с занятия, переделайте согласно этому заданию). 
+
+    
+В HumanController добавьте action Country(если он у вас уже есть с занятия, переделайте согласно этому заданию). 
 Action Country должен принимать один параметр - name(это название страны). 
 Сделайте так чтоб в виде таблицы выводились все человеки из страны, имя которой было передано в action.
+Тут вам необходимо использовать Query параметр, а не Path(прочитайте первый пункт теории).
+
+В Action Index контроллера HumanController добавьте возможность выводить всех человеков, если параметр id равен нулю,
+в обратном случае выводить человека Id которого равен значению параметра id.
+Переименуйте параметр id в humanId и передавайте его как Query параметр.
+Создайте интерфейс репозитория для работы с моделью Human. 
+
  */
 namespace NewPandemic4.Controllers
 {
@@ -30,5 +39,6 @@ namespace NewPandemic4.Controllers
             ViewData["human"] = _context.GetAllHumans(.ToList();
             return View();
         }
+        public IAc
     }
 }
